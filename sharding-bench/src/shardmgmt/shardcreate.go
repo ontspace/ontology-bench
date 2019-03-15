9f3028d8dc35ef89b44ac0e428d3976eff2cb591
Edmond-Honglei-Cong <conghonglei@gmail.com>
2019-03-15 19:09:34 +0800

    shardmgmt init commit

---
+++ sharding-bench/src/shardmgmt/shardcreate.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	sdk "github.com/ontio/ontology-go-sdk"
+	"github.com/ontio/ontology/common/log"
+	"github.com/ontio/ontology/core/types"
+	"github.com/ontio/ontology/smartcontract/service/native/shardmgmt"
+	"github.com/ontio/ontology/smartcontract/service/native/utils"
+	"github.com/ontspace/ontology-bench/config"
+)
+
+type ShardCreateParam struct {
+	Path          string `json:"path"`
+	ParentShardID uint64 `json:"parent_shard_id"`
+}
+
+func ShardCreate(sdk *sdk.OntologySdk, cfg *config.Config, configFile string) error {
+	data, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		return fmt.Errorf("read config from %s: %s", configFile, err)
+	}
+
+	param := &ShardCreateParam{}
+	if err := json.Unmarshal(data, param); err != nil {
+		return fmt.Errorf("unmarshal shard create param: %s", err)
+	}
+
+	user, ok := getAccountByPassword(sdk, param.Path, cfg.Password)
+	if !ok {
+		return fmt.Errorf("get account failed")
+	}
+
+	tShardId, _ := types.NewShardID(param.ParentShardID)
+	createParam := &shardmgmt.CreateShardParam{
+		ParentShardID: tShardId,
+		Creator:       user.Address,
+	}
+
+	buf := new(bytes.Buffer)
+	if err := createParam.Serialize(buf); err != nil {
+		return fmt.Errorf("failed to ser createshard param: %s", err)
+	}
+	method := shardmgmt.CREATE_SHARD_NAME
+	contractAddress := utils.ShardMgmtContractAddress
+	txHash, err := sdk.Native.InvokeNativeContract(cfg.GasPrice, cfg.GasLimit, user, 0,
+		contractAddress, method, []interface{}{buf.Bytes()})
+	if err != nil {
+		return fmt.Errorf("invokeNativeContract error :", err)
+	}
+	log.Infof("shard create txHash is :%s", txHash.ToHexString())
+	return nil
+}
+
+func getAccountByPassword(sdk *sdk.OntologySdk, path string, pwd string) (*sdk.Account, bool) {
+	wallet, err := sdk.OpenWallet(path)
+	if err != nil {
+		log.Errorf("open wallet error:%s", err)
+		return nil, false
+	}
+	user, err := wallet.GetDefaultAccount([]byte(pwd))
+	if err != nil {
+		log.Errorf("getDefaultAccount error:%s", err)
+		return nil, false
+	}
+	return user, true
+}
--- sharding-bench/src/txgen/main.go
+++ sharding-bench/src/txgen/main.go
@@ -3,35 +3,94 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"math"
+	"os"
+	"time"
+
 	goSdk "github.com/ontio/ontology-go-sdk"
 	"github.com/ontio/ontology-go-sdk/client"
 	"github.com/ontio/ontology-go-sdk/utils"
 	"github.com/ontio/ontology/common"
 	"github.com/ontio/ontology/common/log"
 	"github.com/ontspace/ontology-bench/config"
-	"math"
-	"os"
-	"time"
+)
+
+const (
+	INIT_PRIVATE_NETWORK = "init"
+	TEST_BY_CONFIG       = "test"
+	BALANCE_OF           = "balanceOf"
 )
 
 func main() {
 	log.InitLog(log.InfoLog, log.PATH, log.Stdout)
-	configPath := os.Args[1]
+	if len(os.Args) < 2 {
+		log.Errorf("not enough args")
+	}
+	cmd := os.Args[1]
+	configPath := "config.json"
+	if len(os.Args) >= 3 {
+		configPath = os.Args[2]
+	}
 	cfg, err := config.ParseConfig(configPath)
 	if err != nil {
 		log.Error(err)
 		return
 	}
 	sdk := goSdk.NewOntologySdk()
-	rpcClient := client.NewRpcClient()
-	rpcClient.SetAddress(cfg.Rpc)
-	sdk.SetDefaultClient(rpcClient)
-	var wallet, _ = sdk.OpenWallet(cfg.Wallet)
+	wallet, err := sdk.OpenWallet(cfg.Wallet)
+	if err != nil {
+		log.Errorf("parse wallet err: %s", err)
+		return
+	}
 	account, err := wallet.GetDefaultAccount([]byte(cfg.Password))
 	if err != nil {
 		log.Errorf("get account err: %s", err)
 		return
 	}
+	if cmd == INIT_PRIVATE_NETWORK {
+		consensusAccounts := make([]*goSdk.Account, 0)
+		for _, consensusWallet := range cfg.ConsensusPeerPath {
+			wallet, err := sdk.OpenWallet(consensusWallet[0])
+			if err != nil {
+				log.Errorf("parse consensus wallet err: %s", err)
+				return
+			}
+			account, err := wallet.GetDefaultAccount([]byte(consensusWallet[1]))
+			if err != nil {
+				log.Errorf("get consensus account err: %s", err)
+				return
+			}
+			consensusAccounts = append(consensusAccounts, account)
+		}
+		rpcClient := client.NewRpcClient()
+		rpcClient.SetAddress(cfg.Rpc[0])
+		sdk.SetDefaultClient(rpcClient)
+		config.SetGasPrice(sdk, consensusAccounts, 500)
+		config.WithdrawAsset(sdk, consensusAccounts, account)
+		config.InitOep4(sdk, account, cfg.ContractCodePath)
+	} else if cmd == TEST_BY_CONFIG {
+		testOep4Transfer(cfg, account)
+	} else if cmd == BALANCE_OF {
+		rpcClient := client.NewRpcClient()
+		rpcClient.SetAddress(cfg.Rpc[0])
+		sdk.SetDefaultClient(rpcClient)
+		addr := account.Address
+		if len(os.Args) >= 4 {
+			argAddr, err := utils.AddressFromBase58(os.Args[3])
+			if err != nil {
+				log.Errorf("decode arg %s to address failed, err: %s", os.Args[3], err)
+			}
+			addr = argAddr
+		}
+		balanceOf(cfg, sdk, addr)
+	} else {
+		log.Errorf("un support cmd")
+		return
+	}
+
+}
+
+func testOep4Transfer(cfg *config.Config, account *goSdk.Account) {
 	txNum := cfg.TxNum * cfg.TxFactor
 	if txNum > math.MaxUint32 {
 		txNum = math.MaxUint32
@@ -50,13 +109,18 @@ func main() {
 	exitChan := make(chan int)
 	txNumPerRoutine := txNum / cfg.RoutineNum
 	tpsPerRoutine := int64(cfg.TPS / cfg.RoutineNum)
+	startTestTime := time.Now().UnixNano() / 1e6
 	for i := uint(0); i < cfg.RoutineNum; i++ {
-		go func(nonce uint32, fileIndex uint) {
-			startTime := time.Now().Unix()
+		go func(nonce uint32, routineIndex uint) {
+			sendTxSdk := goSdk.NewOntologySdk()
+			rpcClient := client.NewRpcClient()
+			rpcClient.SetAddress(cfg.Rpc[int(routineIndex)%len(cfg.Rpc)])
+			sendTxSdk.SetDefaultClient(rpcClient)
+			startTime := time.Now().UnixNano() / 1e6 // ms
 			sentNum := int64(0)
 			var fileObj *os.File
 			if cfg.SaveTx {
-				fileObj, err = os.OpenFile(fmt.Sprintf("invoke_%d.txt", fileIndex),
+				fileObj, err = os.OpenFile(fmt.Sprintf("invoke_%d.txt", routineIndex),
 					os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
 				if err != nil {
 					fmt.Println("Failed to open the file", err.Error())
@@ -64,31 +128,31 @@ func main() {
 				}
 			}
 			for j := uint(0); j < txNumPerRoutine; j++ {
-				mutTx, err := sdk.NeoVM.NewNeoVMInvokeTransaction(cfg.GasLimit, cfg.GasLimit, contractAddress, params)
+				mutTx, err := sendTxSdk.NeoVM.NewNeoVMInvokeTransaction(cfg.GasPrice, cfg.GasLimit, contractAddress, params)
 				if err != nil {
 					fmt.Println("contract tx err", err)
 					os.Exit(1)
 				}
 				mutTx.Nonce = nonce
-				err = sdk.SignToTransaction(mutTx, account)
+				err = sendTxSdk.SignToTransaction(mutTx, account)
 				if err != nil {
 					log.Errorf("sign tx failed, err: %s", err)
 					continue
 				}
 				if cfg.SendTx {
-					hash, err := sdk.SendTransaction(mutTx)
+					hash, err := sendTxSdk.SendTransaction(mutTx)
 					if err != nil {
 						log.Errorf("send tx failed, err: %s", err)
 					} else {
 						log.Infof("send tx %s", hash.ToHexString())
 					}
 					sentNum++
-					now := time.Now().Unix()
-					diff := sentNum - (now-startTime)*tpsPerRoutine
+					now := time.Now().UnixNano() / 1e6 // ms
+					diff := sentNum - (now-startTime)/1e3*tpsPerRoutine
 					if now > startTime && diff > 0 {
 						sleepTime := time.Duration(diff*1000/tpsPerRoutine) * time.Millisecond
 						time.Sleep(sleepTime)
-						log.Infof("sleep %d ms", sleepTime)
+						log.Infof("sleep %d ms", sleepTime.Nanoseconds()/1e6)
 					}
 				}
 				nonce++
@@ -105,9 +169,30 @@ func main() {
 				}
 			}
 			exitChan <- 1
-		}(uint32(txNumPerRoutine*i), i)
+		}(uint32(txNumPerRoutine*i)+cfg.StartNonce, i)
 	}
 	for i := uint(0); i < cfg.RoutineNum; i++ {
 		<-exitChan
 	}
+	endTestTime := time.Now().UnixNano() / 1e6
+	log.Infof("send tps is %f", float64(txNum*1000)/float64(endTestTime-startTestTime))
+}
+
+func balanceOf(cfg *config.Config, sdk *goSdk.OntologySdk, address common.Address) {
+	contractAddr, err := utils.AddressFromHexString(cfg.Contract)
+	if err != nil {
+		log.Errorf("balanceOf: decode contract addr failed, err: %s", err)
+		return
+	}
+	preResult, err := sdk.NeoVM.PreExecInvokeNeoVMContract(contractAddr, []interface{}{"balanceOf", []interface{}{address}})
+	if err != nil {
+		log.Errorf("balanceOf: pre-execute failed, err: %s", err)
+		return
+	}
+	balance, err := preResult.Result.ToInteger()
+	if err != nil {
+		log.Errorf("balanceOf: parse result %v failed, err: %s", preResult, err)
+		return
+	}
+	log.Infof("balanceOf: addr %s is %d", address.ToBase58(), balance)
 }
